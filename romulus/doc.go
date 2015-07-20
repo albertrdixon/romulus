ad7983d99b9a422e188dfb51d4ee29bd9ab9bf5b
albertrdixon <albert@timelinelabs.com>
2015-07-20 15:49:49 -0700

    fix doc links

--- romulus/doc.go
+++ romulus/doc.go
@@ -1,8 +1,9 @@
 // Package romulus contains all the logic for monitoring the kubernetes api
 // and registering services with vulcan proxy
 //
-// See https://docs.vulcand.io/index.html for vulcan proxy documentation
-// See https://github.com/GoogleCloudPlatform/kubernetes/tree/master/docs for kubernetes documentation
+// vulcan proxy documentation: https://docs.vulcand.io/index.html
+//
+// kubernetes documentation: https://github.com/GoogleCloudPlatform/kubernetes/tree/master/docs
 package romulus
 
 var version = "v0.1.0"
