713dc5a486027e07ee68da5cdf5d9d278a147424
albertrdixon <albert@dixon.rocks>
2015-10-13 14:31:51 -0700

    fix logging

--- romulusd/logger.go
+++ romulusd/logger.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/logutils"
 )
 
-var logPrefix = "[romulusd]"
+var logPrefix = "[romulusd] "
 var logLevels = []string{"fatal", "error", "warn", "info", "debug"}
 
 func setupLog() {
@@ -25,23 +25,23 @@ func setupLog() {
 	log.SetPrefix(logPrefix)
 }
 
-func writeLog(f string, m ...interface{}) {
-	if m == nil || len(m) < 1 {
-		log.Println(f)
-	} else {
-		log.Printf(f, m)
+func writeLog(p, f string, m ...interface{}) {
+	var msg = f
+	if m != nil && len(m) > 0 {
+		msg = fmt.Sprintf(f, m...)
 	}
+	log.Printf("[%s] %s", p, msg)
 }
 
-func errorL(f string, m ...interface{}) { writeLog("[error] %s", fmt.Sprintf(f, m)) }
-func warnL(f string, m ...interface{})  { writeLog("[warn] %s", fmt.Sprintf(f, m)) }
-func infoL(f string, m ...interface{})  { writeLog("[info] %s", fmt.Sprintf(f, m)) }
+func errorL(f string, m ...interface{}) { writeLog("error", f, m...) }
+func warnL(f string, m ...interface{})  { writeLog("warn", f, m...) }
+func infoL(f string, m ...interface{})  { writeLog("info", f, m...) }
 func debugL(f string, m ...interface{}) {
 	if *debug || *logLevel == "debug" {
-		writeLog("[debug] %s", fmt.Sprintf(f, m))
+		writeLog("debug", f, m...)
 	}
 }
 func fatalL(f string, m ...interface{}) {
-	writeLog("[fatal]", fmt.Sprintf(f, m))
+	writeLog("fatal", f, m...)
 	os.Exit(1)
 }
